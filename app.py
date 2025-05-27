e5e280673e18871e5a394cb7dd6415f9007bf6a4
Lex O'brien <lex.e.obrien@gmail.com>
2025-05-27 14:44:59 +0100

    skeleton app ready

--- app.py
+++ app.py
@@ -5,6 +5,24 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
+import numpy as np
+
+
+def digitize(df, col, limit) -> pd.Categorical: 
+    pass
+
+def describe_digitized(y_digitized) -> pd.DataFrame:
+    pass
+
+def suggest_y_range(df, col): 
+    buffer = .1
+    y_vals = df[col].values
+    y_vals_no_nan = y_vals[~np.isnan(y_vals)]
+    y_min, y_max = y_vals_no_nan.min(), y_vals_no_nan.max()
+    delta = buffer*(y_max-y_min)
+    y_min_min = y_min - delta
+    y_max_max = y_max + delta
+    return y_min_min, y_max_max, (y_min, y_max)
 
     
 def validate_input(df): 
@@ -13,8 +31,18 @@ def validate_input(df):
                (df.shape[0] >= 2)
     return is_valid
 
-uploaded_file = st.file_uploader("Choose a file")
+def run_model(df):
+    y_col = st.selectbox('Select response variable', list(df.select_dtypes('float').columns))
+    y_range_suggestion = suggest_y_range(df, y_col)
+    y_range = st.slider("Select Experiment Sensitivity", *y_range_suggestion)
+    y_digitized = digitize(df, y_col, y_range)
+    y_description = describe_digitized(y_digitized)
+    st.dataframe(y_description)
+
+
+uploaded_file = st.file_uploader("Choose a file", )
 if uploaded_file is not None:
+
     raw_data = pd.read_csv(uploaded_file)
 
     input_is_valid = validate_input(raw_data)
@@ -25,6 +53,6 @@ if uploaded_file is not None:
     else: 
         st.write(f"Invalid input")
 
-
+    run_model(raw_data)
 
 
